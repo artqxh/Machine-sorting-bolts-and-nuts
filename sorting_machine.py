b841d6d0cd2c99347ad3b7fe457d1965d62eb488
artqxh <artqxh@gmail.com>
2021-12-13 23:13:21 +0100

    processing one image

--- sorting_machine.py
+++ sorting_machine.py
@@ -132,3 +132,18 @@ plt.title('Strata trenowania i walidacji')
 plt.legend()
 
 plt.show()
+
+
+# Processing one image
+import numpy as np
+from keras.preprocessing import image
+
+img_path = '/home/artur/python_projects/Machine-sorting-bolts-and-nuts/image6.jpg'
+img = image.load_img(img_path, target_size=(150, 150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255
+print(img_tensor.shape)
+
+plt.imshow(img_tensor[0])
+plt.show()
