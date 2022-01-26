cb0a7f1b2174ff1f81237e7b292329411c7f877c
artqxh <artqxh@gmail.com>
2022-01-26 13:56:06 +0100

    modifications with camera added

--- processing_one_image.py
+++ processing_one_image.py
@@ -1,23 +1,43 @@
+import cv2
 import numpy as np
-from PIL import Image
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from PIL import Image
 from tensorflow import keras
 from keras.preprocessing import image
 
-img_path = '/home/artur/python_projects/Machine-sorting-bolts-and-nuts/tests/test1.jpg'
+
+cap = cv2.VideoCapture(-1)
+ret, frame = cap.read()
+
+def takePicture():
+    (grabbed, frame) = cap.read()
+    showimg = frame
+    cv2.waitKey(1)
+    image = 'result.png'
+    cv2.imwrite(image, frame)
+    cap.release()
+    return image
+
+print(takePicture())
+
+
+img = Image.open("result.png")
+
+def zoom_at(img, x, y, zoom):
+    w, h = img.size
+    zoom2 = zoom * 2
+    img = img.crop((x - w / zoom2, y - h / zoom2,
+                    x + w / zoom2, y + h / zoom2))
+    return img.resize((w, h), Image.LANCZOS)
+
+img = zoom_at(img, 345, 245, 3.5)
+img = img.save('result.png')
+
+
+img_path = '/home/artur/python_projects/Machine-sorting-bolts-and-nuts/results.png'
 model_path = '/home/artur/python_projects/Machine-sorting-bolts-and-nuts/bolts_and_nuts_small_1.h5'
 
-
-# Converting colors
-img = Image.open(img_path).convert('L')
-
-npim = np.array(img)
-res = np.zeros((npim.shape[0], npim.shape[1], 3), dtype=np.uint8)
-res[npim < 245] = [150, 150, 150]
-Image.fromarray(res).save('result.png')
-
-img_path = 'result.png'
 
 model = keras.models.load_model(model_path)
 img_size = (150, 150)
@@ -36,5 +56,10 @@ img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255
 
 plt.imshow(img_tensor[0])
+bolt = 100 * (1 - score)
+nut = 100 * score
+print(bolt, nut)
+
+
 plt.title('%.2f percent bolt and %.2f percent nut' % (100 * (1 - score), 100 * score))
 plt.show()
